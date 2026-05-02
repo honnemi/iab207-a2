fbcd19ae19c661c26416428ea07c8f5ece17cbee
honnemi <angelicasiv3@outlook.com>
2026-05-02 14:01:28 +1000

    added basic payment form for checkout page, added order_confirmation.html and coded basic layout for order confirmation page, resolved all routes for payment flow

--- projectfile/website/forms.py
+++ projectfile/website/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, EmailField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from wtforms.widgets import EmailInput
 
@@ -20,3 +20,10 @@ class RegisterForm(FlaskForm):
 
     # submit button
     submit = SubmitField("Register")
+
+class CheckoutForm(FlaskForm):
+    card_number = StringField('Card Number', render_kw={"placeholder": "Enter 16-digit card number"}, validators=[InputRequired(), Length(min=16, max=16)])
+    expiry = StringField('Expiry', render_kw={"placeholder": "MM/YY"}, validators=[InputRequired(), Length(min=5, max=5)])
+    cvv = StringField('CVV', render_kw={"placeholder": "Enter 3 or 4-digit security code"}, validators=[InputRequired(), Length(min=3, max=4)])
+    submit = SubmitField('Complete Payment')
+    # Still need to add proper validation for all fields
--- projectfile/website/tickets.py
+++ projectfile/website/tickets.py
@@ -1,11 +1,26 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap5
+from .forms import CheckoutForm
 
 tickets_bp = Blueprint('tickets', __name__)
 
-@tickets_bp.route('/tickets')
+@tickets_bp.route('/tickets', methods = ['GET'])
 def show_tickets():
     return render_template('tickets.html')
 
-@tickets_bp.route('/tickets/checkout')
+@tickets_bp.route('/tickets/checkout', methods = ['GET', 'POST'])
 def show_checkout():
-    return render_template('checkout.html')
+    form = CheckoutForm()
+
+    if form.validate_on_submit():
+        card_number = form.card_number.data
+        expiry = form.expiry.data
+        cvv = form.cvv.data
+
+        return redirect(url_for('tickets.show_confirmation'))
+
+    return render_template('checkout.html', form=form)
+
+@tickets_bp.route('/tickets/confirmation', methods = ['GET'])
+def show_confirmation():
+    return render_template('order_confirmation.html')
