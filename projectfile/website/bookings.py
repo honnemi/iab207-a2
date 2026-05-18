631ffb8c46cbd251fc81afc9591aa3735a02a1eb
honnemi <angelicasiv3@outlook.com>
2026-05-18 15:58:18 +1000

    created booking history page which retrieves bookings from the database

--- projectfile/website/bookings.py
+++ projectfile/website/bookings.py
@@ -6,11 +6,10 @@ bookings_bp = Blueprint('bookings', __name__)
 
 @bookings_bp.route('/bookings', methods = ['GET'])
 def show_bookings():
-    return render_template('booking_history.html')
+    bookings = Booking.query.all()
+    return render_template('booking_history.html', bookings=bookings)
 
 @bookings_bp.route('/order_confirmation/<int:booking_id>', methods = ['GET'])
 def show_order_confirmation(booking_id):
     booking = Booking.query.get_or_404(booking_id)
-    user = booking.user
-    event = booking.event
-    return render_template('order_confirmation.html', booking=booking, user=user, event=event)
+    return render_template('order_confirmation.html', booking=booking)
